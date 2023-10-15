47323d87859676f47ee5ec2e9f15b6e313f3a24e
halidak <halida.karisik6@gmail.com>
2023-10-15 18:54:06 +0200

    init

---
+++ src/main/java/com/example/pmaapi/PmaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pmaapi/PmaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pmaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
